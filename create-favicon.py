1da0bbb428b1f1be887162c2a59b8984f1d53d9c
Fatih Erikli <fatiheriklicomputergraphics@gmail.com>
2024-11-30 00:52:29 +0300

    Add cloud texture gen

--- create-favicon.py
+++ create-favicon.py
@@ -4,17 +4,27 @@ def px(x, y):
   return "{x}:{y}".format(x=x, y=y)
 def draw(size, scr):
   out = []
-  for y in range(0, size*2):
+  for y in range(0, size):
     row = []
-    for x in range(0, size*2):
-      row.append(scr.get(px(x - size, -1*(y - size)), (255, 255, 255)))
+    for x in range(0, size):
+      row.append(scr.get(px(x, y), (255, 255, 255)))
     out.append(row)
   return out
-size = 64
+size = 128
 scr = {}
-for x in range(-64, 64):
-  for y in range(-64, 65):
-    scr[px(x, y)] = random.choice([[230,230,230], [255,255,255], [255,255,255], [255,255,255]])
+grid = 64
+cellsize = int(size / grid)
+cells = []
+for y in range(grid):
+  row = []
+  for x in range(grid):
+    row.append(random.choice([[230,230,230], [255, 255, 255], [255, 255, 255], [255, 255, 255], [255, 255, 255]]))
+  cells.append(row)
+for row, cols in enumerate(cells):
+  for col, cell in enumerate(cols):
+    for x in range(0, cellsize):
+      for y in range(0, cellsize):
+        scr[px(col*cellsize+x, row*cellsize+y)] = cell
 png = draw(size, scr)
 f = open("favicon.png", "wb")
 dump_png(f, png)
