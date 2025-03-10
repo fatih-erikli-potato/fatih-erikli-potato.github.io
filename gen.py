5eaa5d538294c3b3fbc1e4999cb23f38175ef825
Fatih Erikli <fatiheriklicomputergraphics@gmail.com>
2025-03-10 04:41:05 +0300

    Add computer

--- gen.py
+++ gen.py
@@ -1,8 +1,8 @@
-from html import make_html_w_doctype, div
+from html import make_html_w_doctype, div, img
 f = open("index.html", "w")
 html = make_html_w_doctype(
   {"title": "Fatih Erikli", "stylesheet_url": "/style.css", "favicon_url": "/favicon.png"},
-  div({"class": "container"}, "Homepage")
+  div({"class": "bio"}, "Fatih Erikli")
 )
 f.write(html)
 f.close()
