10fbbf910b09d1dc1df3ac76a998ca99d299429b
fatih-erikli-potato <fatiheriklicomputergraphics@gmail.com>
2025-06-27 01:34:38 +0300

    Path error

--- gen.py
+++ gen.py
@@ -1,7 +1,7 @@
 from html import make_html_w_doctype, div, img
 f = open("index.html", "w")
 html = make_html_w_doctype(
-  {"title": "Font Renderer", "favicon_url": "/favicon.png", "scripts": ["/font.js"]},
+  {"title": "Font Renderer", "favicon_url": "favicon.png", "scripts": ["font.js"]},
   div({"class": "container"})
 )
 f.write(html)
