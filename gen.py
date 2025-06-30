919956cffdda5653b67a84598899dd59a80ee2b7
fatih-erikli-potato <fatiheriklicomputergraphics@gmail.com>
2025-06-30 15:40:41 +0300

    render

--- gen.py
+++ gen.py
@@ -1,8 +1,11 @@
 from html import make_html_w_doctype, div, img
 f = open("index.html", "w")
 html = make_html_w_doctype(
-  {"title": "Font Renderer", "favicon_url": "favicon.png", "scripts": ["font.js"]},
-  div({"class": "container"})
+  {"title": "Font Renderer", "favicon_url": "favicon.png",
+  "styles": ["style.css"],
+  "scripts": ["font.js"]},
+  div({"class": "container"}),
+  div({"class": "render"}, img({"src": "cup-of-coffee.png", "width": "256px", "height": "256px"}))
 )
 f.write(html)
 f.close()
