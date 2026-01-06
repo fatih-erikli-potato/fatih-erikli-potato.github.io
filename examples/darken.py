e69b9a52b34f9c7983f66693134bbcec0e41108d
fatih-erikli-potato <fatiheriklicomputergraphics@gmail.com>
2026-01-06 09:13:06 +0300

    update html website

--- examples/darken.py
+++ examples/darken.py
@@ -2,4 +2,4 @@ def darken(r, g, b, percent):
   rblacks = int(r * (percent/100))
   gblacks = int(g * (percent/100))
   bblacks = int(b * (percent/100))
-  return (max(r - rblacks, 0), max(g - gblacks, 0), max(b - bblacks, 0))
+  return (r - rblacks, g - gblacks, b - bblacks)
